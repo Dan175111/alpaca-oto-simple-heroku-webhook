7ca163fe60049d4684dedfd09f29a0fe7d7bcca5
Dan Maloney <Dan175111@users.noreply.github.com>
2021-03-20 13:54:19 -0700

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,3 @@ API_KEY = 'your_key'
 API_SECRET = 'your_secret_key'
 
 WEBHOOK_PASSPHRASE = 'your_passphrase'
-DISCORD_WEBHOOK_URL = False # use a string containing your discord webhook url to enable
